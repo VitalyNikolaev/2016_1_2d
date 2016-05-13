dc3f6d9722d394d9a68c97a10ad1b175f74c265e
Paul Maminov <xobotun@ya.ru>
2016-05-14 00:21:11 +0300

    Even though server.js is deprecated, I updated it...

--- server.js
+++ server.js
@@ -25,27 +25,27 @@ app.use(function (req, res, done) {
 app.put('/api/session', function(req, res) {
 	var username = req.body.login;
 	var password = req.body.password;
-	var newurl = 'http://localhost:8081/api/session/';
+	var newurl = 'http://localhost:80/api/session/';
 	request({ url: newurl, method: 'PUT', json: {login: username, password: password}}).pipe(res);
 });
 
 app.put('/api/user', function(req, res) {
 	var login = req.body.login;
 	var password = req.body.password;
-	var newurl = 'http://localhost:8081/api/user';
+	var newurl = 'http://localhost:80/api/user';
 	request({ url: newurl, method: 'PUT', json: {login: login, password: password}}).pipe(res);
 });
 
 app.delete('/api/session', function(req, res) {
-	var newurl = 'http://localhost:8081/api/session';
+	var newurl = 'http://localhost:80/api/session';
 	request({ url: newurl, method: 'DELETE'}).pipe(res);
 });
 
 app.get('/api/session', function(req, res) {
-	req.pipe(request('http://localhost:8081' + req.url)).pipe(res);
+	req.pipe(request('http://localhost:80' + req.url)).pipe(res);
 });
 app.get('/api/user/*',function(req,res) {
-	req.pipe(request('http://localhost:8081' + req.url)).pipe(res);
+	req.pipe(request('http://localhost:80' + req.url)).pipe(res);
 });
 app.listen(PORT, function () {
 	console.log("listening at http://%s:%s", HOSTNAME, PORT);
