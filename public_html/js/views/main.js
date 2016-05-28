f99170d72875ce4c651ef68ef23abeb72fc3324a
Vitaly Nikolaev <vit3011@rambler.ru>
2016-05-28 10:49:55 +0300

    guest ready

--- public_html/js/views/main.js
+++ public_html/js/views/main.js
@@ -2,6 +2,7 @@ define(function (require) {
         var baseView = require('views/baseView');
         var tmpl = require('tmpl/main');
         var app = require('app');
+        var $ = require('jquery');
 
         var View = baseView.extend({
             template: tmpl,
@@ -15,6 +16,10 @@ define(function (require) {
                             self.reloadView();
                         }
                     });
+                },
+                'click #asGuest': function(e) {
+                    e.preventDefault();
+                    this.makeGuestAccout();
                 }
             },
             initialize: function () {
@@ -23,6 +28,24 @@ define(function (require) {
                 this.listenTo(app.Events, "showError", this.showErrorMessage);
 
             },
+            makeGuestAccout: function () {
+                $.ajax({
+                    url: 'http://uinames.com/api/'
+                }).done(function (data) {
+                    app.user.save({login: data.name, password: data.name}, {
+                        success: function () {
+                            app.session.set('authed', true);
+                            app.user.fetch({
+                                success: function () {
+                                    app.Events.trigger('userAuthed');
+                                    window.location.href = '#main'
+                                }
+                            });
+                        }
+                    });
+                });
+            },
+
             reloadViewWithAuthTemplate: function() {
                 app.fetchUserAvatarOrGetRandom();
                 this.template = require('tmpl/main_authed');
